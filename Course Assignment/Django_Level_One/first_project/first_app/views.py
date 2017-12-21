ca792a317170a1595c14ca9f4658403e15a1745e
Minghao Shan <minghao8shan@gmail.com>
2017-12-21 14:45:53 -0600

    upload Django

--- Course Assignment/Django_Level_One/first_project/first_app/views.py
+++ Course Assignment/Django_Level_One/first_project/first_app/views.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me':"Now I am coming from first_app/index.html!"}
-    return render(request,'first_app/index.html',context=my_dict)
+    return HttpResponse("Hello world. You are at the polls index.")
