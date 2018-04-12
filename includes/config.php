c8b00379e87b8448e51a8338865addd21fb0e45a
Minghao Shan <minghao8shan@gmail.com>
2018-04-12 17:08:03 -0500

    refactoring & working on You Might Like page

--- includes/config.php
+++ includes/config.php
@@ -6,6 +6,7 @@
 
     $timezone = date_default_timezone_set("America/Chicago");
 
+    // $con = mysqli_connect("mysql.truman.edu", "ms1723", "eemahdam", "ms1723CS315");
     $con = mysqli_connect("localhost", "root", "", "+1s");
 
     if(mysqli_connect_errno()) {
---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<?php
+
+include "includes/config.php";
+
+//if the user is not logged in, direct user to register page
+if (isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+    header("Location:register.php");
+}
+
+?>
+
+    <html>
+
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>+1s</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="assets/img/icon.png" />
+        <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
+        <script src="main.js"></script>
+    </head>
+
+    <body>
+        <div id="mainContainer">
+        
+            <div id="topContainer">
+                <?php include("includes/navBarContainer.php");?>
+
+                <div id="mainViewContainer">
